0424f2e123a3e48fb7fd69e8cdaef539d958f165
Jakub Parlej <52372627+jparlej@users.noreply.github.com>
2021-03-28 18:06:36 +0200

    Add scroll functions

---
+++ scripts/main.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Hello world");
+
+    const rootElement = document.querySelector('#root');
+    const sections = document.querySelectorAll('section');
+    let currentSectionIndex = 0;
+    let isThrottled = false;
+
+    sections[currentSectionIndex].scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+    })
+
+    document.addEventListener('wheel', (event) => {
+        if (isThrottled) return;
+        isThrottled = true;
+
+        setTimeout(() => {
+            isThrottled = false;
+        }, 500)
+
+        const direction = event.deltaY > 0 ? 1 : -1;
+
+        if (direction === 1) {
+            const isLastSection = currentSectionIndex === sections.length - 1;
+            if (isLastSection) return;
+        } else if (direction === -1) {
+            const firstSection = currentSectionIndex === 0;
+            if (firstSection) return;
+        }
+
+        currentSectionIndex = currentSectionIndex + direction;
+
+        sections[currentSectionIndex].scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        })
+    })
+
+
+})
